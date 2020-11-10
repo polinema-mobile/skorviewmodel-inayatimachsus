df092073c8e721f9b64437e7ddbbfdcd91a8403a
inayatimachsus <inayati.mia@gmail.com>
2020-11-10 12:37:11 +0700

    Praktikum

--- app/src/main/java/id/ac/polinema/skorviewmodel/fragments/GoalFragment.java
+++ app/src/main/java/id/ac/polinema/skorviewmodel/fragments/GoalFragment.java
@@ -38,7 +38,7 @@ public class GoalFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		FragmentGoalBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_goal, container, false);
-		// instance viewmodel here
+		viewModel = new ViewModelProvider(requireActivity()).get(ScoreViewModel.class);
 		binding.setFragment(this);
 		binding.setGoalScorer(goalScorer);
 		requestKey = GoalFragmentArgs.fromBundle(getArguments()).getRequestKey();
@@ -49,7 +49,7 @@ public class GoalFragment extends Fragment {
 		Bundle bundle = new Bundle();
 		bundle.putParcelable(ScoreFragment.SCORER_KEY, goalScorer);
 
-		// set goalscorer use viewmodel
+		viewModel.setGoalScorer(requestKey, goalScorer);
 		Navigation.findNavController(view).navigateUp();
 	}
 
